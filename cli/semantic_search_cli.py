0cd478818439ce97cd1f55383a821c776f050a4c
Sterling Swygert <sterling.swygert+pearson@pearson.com>
2025-12-24 17:54:30 -0500

    (feat): Added embedding functionality to semantic search cli

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,7 +1,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -9,11 +9,16 @@ def main():
 
     verify_parser = subparsers.add_parser("verify", help="Verify the model loads correctly")
 
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embeds text using the semantic search model")
+    embed_text_parser.add_argument("text", type=str, help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
